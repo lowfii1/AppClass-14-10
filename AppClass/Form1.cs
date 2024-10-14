66a91f36834855d3b207a6d7f022c2af2970a17e
SORAIA SILVA DE SOUZA <soraia.ssouza2@senacsp.edu.br>
2024-10-14 15:45:41 -0300

    Adicionar arquivos de projeto.

---
+++ AppClass/Form1.cs
@@ -0,0 +1,10 @@
+namespace AppClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AppClass/Usuario.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppClass
+{
+    // classe de análise ou de projeto
+     public class Usuario: Escola
+    {
+        // atributos
+
+        // propriedades
+
+        // métodos construtores
+
+        // métodos de projeto
+    }
+    public abstract class Escola
+    {
+        // atributos
+
+        // propriedades (métodos de acesso)
+
+        // métodos construtores
+
+        // métodos de projeto
+    }
+}
